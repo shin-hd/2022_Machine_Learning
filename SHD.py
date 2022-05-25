9578fb94d6d3efa31c944c2d158c434165d969df
shin-hd <shinhd1998@gmail.com>
2022-05-25 13:14:11 +0900

    refactoring

--- SHD.py
+++ SHD.py
@@ -12,7 +12,7 @@ from sklearn.cluster import KMeans
 
 # Seed setting
 seed_num = 2022
-#np.random.seed(seed_num)
+np.random.seed(seed_num)
 iteration = 100     # Number of times to repeat steps E and M.
 
 
@@ -47,137 +47,200 @@ class EM:
         self.sigma = np.zeros((3,4,4)) 
         self.pi = np.zeros((3))
         
-    def initialization(self, z): # your parameter here): 
+    def initialization(self, data, z):
         """ 1.initialization, 10 points
         Initial values for mean, sigma, and pi should be assigned.
         It have a significant impact on performance.
         
-        your comment here
-        """
-        # your code here
-        
-        r, _ = np.shape(data)
-        c = int(r / self.n_clusters) # n_data / n_clusters
-        
-        # 각 cluster별 mean, covariance, pi 계산
-        for k in range(self.n_clusters - 1):
-            z[k*c:(k+1)*c, k] = [1 for _ in range(k*c, (k+1)*c)]
-            self.mean[k] = np.mean(data[k*c:(k+1)*c], 0)
-            
-            mean_centered = data[k*c:(k+1)*c] - self.mean[k]
-            self.sigma[k] = np.transpose(mean_centered).dot(mean_centered) / c
-            
-            self.pi[k] = c/r
+        데이터를 임의의 cluster에 배정하고,
+        각 cluster별 mean, sigma, pi를 계산.
 
-        z[(self.n_clusters-1)*c:, self.n_clusters-1] = [1 for _ in range((self.n_clusters-1)*c, r)]
-        self.mean[self.n_clusters - 1] = np.mean(data[(self.n_clusters-1)*c:], 0)
+        Parameters
+        ----------
+        data (ndarray): Input dataset.
+        z (ndarray): Data of each cluster.
+        """
+        r, _ = np.shape(data) # data 수
         
-        mean_centered = data[(self.n_clusters-1)*c:] - self.mean[self.n_clusters-1]
-        self.sigma[self.n_clusters - 1] = np.transpose(mean_centered).dot(mean_centered) / (r-(self.n_clusters-1)*c)
+        # z[n]을 n_clusters에 균등하게 배정
+        for n in range(r):
+            z[n, n%self.n_clusters] = 1
+
+        # shuffle해서 초기 cluster 랜덤
+        np.random.shuffle(z)
         
-        self.pi[self.n_clusters - 1] = (r-(self.n_clusters-1)*c) / r
-        
+        # cluster별 mean, sigma, pi 계산
+        for k in range(self.n_clusters):
+            self.mean[k] = np.mean(data[np.where(z[:,k] == 1)], 0)
+            
+            mean_centered = data[np.where(z[:,k] == 1)] - self.mean[k]
+            self.sigma[k] = mean_centered.T.dot(mean_centered) / (np.sum(z[:,k])-1)
+            #self.sigma[k] = (np.cov(data[np.where(z[:,k] == 1)].T))
+            
+            self.pi[k] = np.sum(z[:,k])/r
+
         #print(self.mean)
         #print(self.sigma)
+        #print(mean_centered)
         #print(np.cov(np.transpose(mean_centered)))
-        #print(self.pi)
-        
+        #print(self.pi)        
 
-        return # something or nothing
+        return z
             
-    def multivariate_gaussian_distribution(self, data, k, z): # your parameter here):
+    def multivariate_gaussian_distribution(self, data, k, z):
         """ 2.multivariate_gaussian_distribution, 10 points
         Use the linear algebraic functions of Numpy. π of this function is not self.pi
         
-        your comment here
+        입력 data에 대한 multivariate normal distribution 계산.
+
+        Parameters
+        ----------
+        data (ndarray): Input data.
+        k (int): Cluster of input data.
+        z (ndarray): Data of each cluster.
+
+        Returns
+        -------
+        g (scalar): mvn result.
         """
-        # your code here
-        # 전체 dataset에 대한 cluster별 mgd 계산
-        # np.linalg.inv
-        exp = np.exp((-0.5) * (data-self.mean[k]).dot(np.linalg.inv(self.sigma[k])).dot(np.transpose(data-self.mean[k])))
-        denom = ((2 * np.pi) ** (1.0 / sum(z[:,k]))) * np.sqrt(np.linalg.det(self.sigma[k]))
-        #print(((2 * np.pi) ** (1.0 / len(cluster)), np.sqrt(np.linalg.det(self.sigma[i]))))
+        
+        # 분자부분
+        d_m = data-self.mean[k] # 1x4, 데이터값-평균값
+        inv = np.linalg.inv(self.sigma[k]) # 4x4, inverse of covariance matrix
+        exp = np.exp((-0.5) * (d_m.dot(inv).dot(d_m.T))) # 1x1 = 1x4 . 4x4 . 4x1
+        # 분모부분
+        det = np.linalg.det(self.sigma[k])
+        denom = np.power((2 * np.pi), np.sum(z[:,k])/2) * np.sqrt(det)
+        
+        # mvn 계산
         g =  exp / denom
         
-        return g# something or nothing
+        return g
     
-    def expectation(self, z): # your parameter here):
+    def expectation(self, data, z):
         """ 3.expectation step, 20 points
         The multivariate_gaussian_distribution(MVN) function must be used.
         
-        your comment here
+        입력 data의 cluster별 posterior 계산. 
+
+        Parameters
+        ----------
+        data (ndarray): Input data.
+        z (ndarray): Data of each cluster.
+
+        Returns
+        -------
+        y (ndarray): data의 cluster에 대한 posterior(NxK).
         """
-        # your code here
-        # data별로 각 cluster에 대한 expectation 계산
-        r, _ = np.shape(data)
+        r, _ = np.shape(data) # N
         y = np.zeros((r, self.n_clusters)) # N x K
-        # for k in range(self.n_clusters):
-        #     for n in range(r):
+        
+        # data의 cluster별 posterior 계산
         for n in range(r):
             for k in range(self.n_clusters):
                 g = self.multivariate_gaussian_distribution(data[n], k, z)
-                y[n][k] = self.pi[k] * g
+                y[n,k] = self.pi[k] * g
+                
+                # z 초기화
+                z[n,k] = 0
+
             y[n] = y[n]/sum(y[n])
-        
-        for n in range(r):
-            for k in range(self.n_clusters):
-                z[n][k] = 0
+
+            # posterior가 최대가 되는 cluster에 재배정
             z[n, np.argmax(y[n])] = 1
+        
+        return y
 
-        return y# something or nothing
-
-    def maximization(self, y, z): # your parameter here): 
+    def maximization(self, data, y, z):
         """ 4.maximization step, 20 points
         Hint. np.outer
         
-        your comment here
-        """
-        # your code here
-        # cluster별로 Max한
-        r, _ = np.shape(data)
+        변경된 cluster에 따른 mu, sigma, pi 재계산
         
+        Parameters
+        ----------
+        data (ndarray): Input data.
+        z (ndarray): Data of each cluster.
+        """
+        
+        r, _ = np.shape(data) # N
+        
+        # covariance matrix 초기화
         self.sigma = np.zeros((3,4,4))
+
+        # 각 cluster별로 mu, sigma, pi 재계산
         for k in range(self.n_clusters):
-            denom = sum(y[:,k]*z[:,k])
+            # 분모
+            denom = np.sum(y[np.where(z[:,k] == 1)][:,k])
+
+            # 분자
+            num = np.sum([y[n,k]*data[n] for n in np.where(z[:,k] == 1)[0]], 0)
             
-            num = sum([(y[n,k]*z[n,k]*data[n]) for n in range(r)])
+            # mean 계산
             self.mean[k] = num/denom
 
-            for n in range(r):
-                dev = data[n]-self.mean[k]
-                dev = np.array(dev)[np.newaxis]
-                self.sigma[k] += y[n,k] * z[n,k] * (dev.T).dot(dev)
-                
+            # sigma 계산
+            for n in np.where(z[:,k] == 1)[0]:
+                # transpose 함수 사용 가능하도록 array to matrix
+                d = np.array(data[n]-self.mean[k])[np.newaxis]
+                self.sigma[k] += y[n,k] * np.transpose(d).dot(d)
             self.sigma[k] /= denom
-
-            self.pi[k] = sum([z[n,k]*y[n,k] for n in range(r)])/r
+            
+            # 기댓값으로 pi 계산
+            self.pi[k] = np.sum(y[np.where(z[:,k] == 1)][:,k])/r
 
         #print(self.mean)
         #print(self.sigma)
-        #print(np.cov(np.transpose(mean_centered)))
+        #print(np.cov(np.transpose(data[np.where(z[:,2] == 1)] - self.mean[2])))
         #print(self.pi)
 
-        return # something or nothing
+        return # nothing
         
-    def fit(self): # your parameter here):
+    def fit(self, data):
         """ 5.fit clustering, 20 points
         Functions initialization, expectation, and maximization should be used by default.
         Termination Condition. Iteration is finished or posterior is the same as before. (Beware of shallow copy)
         Prediction for return should be formatted. Refer to iris['target'] format.
         
-        your comment here
-        """
-        # your code here
-        r,_ = np.shape(data)
-        z = np.zeros((r, self.n_clusters))
-        self.initialization(z)
+        GMM으로 학습해서 cluster 분류.
 
-        for _ in range(self.iteration):
-            y = self.expectation(z)
-            self.maximization(y, z)
+        Parameters
+        ----------
+        data (ndarray): Input dataset.
         
-        prediction = [np.argmax(z[i]) for i in range(r)]# np array (150) as assigned by labels 0, 1, 2
-        #print(prediction)
+        Returns
+        -------
+        prediction (ndarray): 예측한 cluster.
+        """
+        r,_ = np.shape(data) # N
+
+        # NxK matrix
+        # data의 cluster면 1, 아니면 0
+        z = np.zeros((r, self.n_clusters)).astype(int) # 0으로 초기화
+
+        # EM 초기화
+        self.initialization(data, z)
+        
+        # z 이전값
+        z_prev = copy.copy(z)
+
+        # 최대 iteration만큼 반복
+        for _ in range(self.iteration):
+            # e step
+            y = self.expectation(data, z)
+            
+            # z가 이전과 같으면 반복 탈출 
+            if (z==z_prev).all():
+                break
+            else: # z_prev 갱신
+                z_prev = copy.copy(z)
+
+            # m step
+            self.maximization(data, y, z)
+        
+        # data의 cluster로 구성된 np array(150)
+        prediction = np.array([np.argmax(z[i]) for i in range(r)])
+        
         return prediction 
 
 def plotting(data:pd.DataFrame):
@@ -191,12 +254,11 @@ def plotting(data:pd.DataFrame):
     -------
     None.
     """
-    # your code here
     # 산점도 행렬을 각 변수별 커널밀도추정곡선을 볼 수 있도록,
     # labels별로 색을 다르게해서, 출력색을 bright로 출력한다.
     sns.pairplot(data, diag_kind='kde', hue="labels", palette='bright')
-    #plt.show(block=True)
-    return # something or nothing
+    plt.show()#block=True)
+    return
     
     
 if __name__ == '__main__':
@@ -204,14 +266,14 @@ if __name__ == '__main__':
     iris = datasets.load_iris()
     original_data = pd.DataFrame(data= np.c_[iris['data'], iris['target']], columns= iris['feature_names'] + ['labels'])
     original_data['labels'] = original_data['labels'].map({0:'setosa', 1:'versicolor', 2:'virginica'})
-    plotting(original_data)
+    #plotting(original_data)
     
     # Only data is used W/O labels beacause EM and Kmeans are unsupervised learning
     data = iris['data']
-    
+
     # Unsupervised learning(clustering) using EM algorithm
     EM_model = EM(n_clusters=3, iteration=iteration)
-    EM_pred = EM_model.fit()# your parameter here)
+    EM_pred = EM_model.fit(data) # EM으로 학습
     EM_pd = pd.DataFrame(data= np.c_[data, EM_pred], columns= iris['feature_names'] + ['labels'])
     plotting(EM_pd)
     
@@ -240,4 +302,3 @@ if __name__ == '__main__':
     KM_hit = np.sum(iris['target']==KM_pred)
     print(f'EM Accuracy: {round(EM_hit / 150,2)}    Hit: {EM_hit} / 150')
     print(f'KM Accuracy: {round(KM_hit / 150,2)}    Hit: {KM_hit} / 150')
-    
